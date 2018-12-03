73db79bc2fa96fab5d0e2192d466d91f06f8d450
Daniil Ivanov <36517601+daniiliv@users.noreply.github.com>
2018-12-03 20:07:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 int main (){
 	setlocale(LC_ALL, "Rus");
 
-	TRAIN *train = new TRAIN;
-	TRAIN trains[8];
+	TRAIN *trains; //это поле класса trainCollection
+	trains = new TRAIN[6]; //это в конструктор класса trainCollection (просто выделяешь место для 6 объектов, не заполняя ничем)
+	//потом перегруженным оператором ввода заполнишь объекты класса train своими данными 
 	TRAIN t;
 	t.setdata();
 	t.get_nameofplace();
